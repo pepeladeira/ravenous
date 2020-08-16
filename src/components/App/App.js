2189fb4ee35581e24113b464837f11ab92474b2a
Pedro Ladeira <pmladeira@live.com>
2020-08-15 22:09:25 -0300

    connect to Yelp API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,37 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-import SearchBar from "./../SearchBar/SearchBar";
-import BusinessList from "./../BusinessList/BusinessList";
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList />
-    </div>
-  );
+import Yelp from '../../util/Yelp';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
+    );
+  }
 }
 
 export default App;
